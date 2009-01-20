7c41011ade3dac72b9981d437de696bfb61a3c11
Rein Henrichs <reinh@reinh.com>
2009-01-20 17:14:22 -0500

    Refactor readers spec to create new classes

--- spec/endeca/readers_spec.rb
+++ spec/endeca/readers_spec.rb
@@ -6,69 +6,70 @@ describe Endeca::Readers do
   end
 
   before do
-    @helper = Helper.new
-    @helper.stub!(:attributes).and_return("helper_id" => "1")
+    @helper = Class.new(Endeca::Document)
+    @a_helper = @helper.new('Properties' => {'helper_id' => "1"})
   end
 
   describe ".add_reader" do
     it "should add a method named for the argument" do
-      Helper.add_reader(:float_reader) {|x| x.to_f}
-      Helper.should respond_to(:float_reader)
+      @helper.add_reader(:float_reader) {|x| x.to_f}
+      @helper.should respond_to(:float_reader)
     end
   end
 
   describe ".reader" do
     describe "with a symbol" do
       it "adds a reader that returns the attribute by that key" do
-        Helper.reader(:helper_id)
-        @helper.should respond_to(:helper_id)
-        @helper.helper_id.should == "1"
+        @helper.reader(:helper_id)
+        @a_helper.should respond_to(:helper_id)
+        @a_helper.helper_id.should == "1"
       end
     end
 
     describe "with a hash" do
       it "adds a reader that returns the corresponding element" do
-        Helper.reader(:helper_id => :new_helper_id)
-        @helper.should respond_to(:new_helper_id)
-        @helper.new_helper_id.should == "1"
+        @helper.reader(:helper_id => :new_helper_id)
+        @a_helper.should respond_to(:new_helper_id)
+        @a_helper.new_helper_id.should == "1"
       end
     end
 
     describe "with a hash and a block" do
       it "adds a reader that returns the corresponding element, cast by calling the block" do
-        Helper.reader(:helper_id => :succ_helper_id){|id| id.succ}
-        @helper.should respond_to(:succ_helper_id)
-        @helper.succ_helper_id.should == "1".succ
+        @helper.reader(:helper_id => :succ_helper_id){|id| id.succ}
+        a_helper = @helper.new('Properties' => {'helper_id' => "1"})
+        a_helper.should respond_to(:succ_helper_id)
+        a_helper.succ_helper_id.should == "1".succ
       end
     end
   end
 
   describe ".integer_reader" do
     it "adds a reader that casts the value to an integer" do
-      Helper.integer_reader(:helper_id)
-      @helper.helper_id.should == 1
+      @helper.integer_reader(:helper_id)
+      @a_helper.helper_id.should == 1
     end
   end
 
   describe ".decimal_reader" do
     it "adds a reader that casts the value to an decimal" do
-      Helper.decimal_reader(:helper_id)
-      @helper.helper_id.should == BigDecimal.new("1")
+      @helper.decimal_reader(:helper_id)
+      @a_helper.helper_id.should == BigDecimal.new("1")
     end
   end
 
   describe ".float_reader" do
     it "adds a reader that casts the value to an float" do
-      Helper.float_reader(:helper_id)
-      @helper.stub!(:attributes).and_return("helper_id" => "1.9234")
-      @helper.helper_id.should == Float("1.9234")
+      @helper.float_reader(:helper_id)
+      a_helper = @helper.new('Properties' => {'helper_id' => "1.9234"})
+      a_helper.helper_id.should == Float("1.9234")
     end
   end
 
   describe ".boolean_reader" do
     it "adds a reader that casts the value to an boolean" do
-      Helper.boolean_reader(:helper_id)
-      @helper.helper_id.should == true
+      @helper.boolean_reader(:helper_id)
+      @a_helper.helper_id.should == true
     end
   end
 end
