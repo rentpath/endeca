4452c0efcc7c5a856a3e47049536dfa9ee691a20
Andrew Stone <andy@stonean.com>
2009-01-30 15:47:57 -0500

    reord reader statements and added record_count

--- lib/endeca/dimension.rb
+++ lib/endeca/dimension.rb
@@ -4,16 +4,19 @@ module Endeca
     extend ClassToProc
     extend Readers
 
+    reader \
+      "DimValueName"  => :name,
+      "SelectionLink" => :to_params
+
+    integer_reader \
+      "DimValueID"      => :id,
+      "NumberofRecords" => :record_count
+
     attr_reader :raw
     def initialize(raw={})
       @raw=raw
     end
     alias_method :attributes, :raw
-
-    reader "DimValueName" => :name
-    reader "PivotLink"    => :to_params
-
-    integer_reader "DimValueID" => :id
 
     def inspect
       "#<#{self.class}=0x#{self.object_id.to_s(16)} id=#{id} name=#{name.inspect}>"
